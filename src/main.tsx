6b2c4f440651073704cc3d2c325fac15824f15c5
R <rizkyramadhanpd@gmail.com>
2025-07-28 22:01:08 +0700

    chore: create path alias and add related dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./globals.css";
-import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./App.tsx";
 import Home from "./pages/home.tsx";
 import Surah from "./pages/surah.tsx";
 import { AudioProvider } from "./context/AudioProvider.tsx";
 import { ModalProvider } from "./context/ModalProvider.tsx";
 import { ThemeProvider } from "./context/ThemeProvider.tsx";
+import "./globals.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src")
+    }
+  }
+});
